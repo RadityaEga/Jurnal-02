d450e13a4454366a2cac35d4bc58b179664e4258
RadityaEga <89286716+RadityaEga@users.noreply.github.com>
2021-10-02 17:42:40 +0700

    HelloWorld.java

---
+++ DemoGit/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama:Raditya");
+		System.out.println("NIM:6706210103");
+		System.out.println("Kelas:D3IF4502");
+	}
+}
